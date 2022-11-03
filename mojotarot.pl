fb29a7d1e199bf3b19ace9c59085c597a4159a80
Gene Boggs <gene@ology.net>
2022-11-03 13:20:12 -0500

    Use a fantasy font

--- mojotarot.pl
+++ mojotarot.pl
@@ -310,6 +310,9 @@ __DATA__
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" onerror="this.onerror=null;this.href='/css/bootstrap.min.css';" />
     <title><%= title %></title>
     <style>
+      body {
+        font-family: luminari, fantasy;
+      }
       .padpage {
         padding-top: 10px;
       }
