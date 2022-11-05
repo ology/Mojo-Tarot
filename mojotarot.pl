3fa141c60e7fbfed3e249a15a40e03a6c986d76b
Gene Boggs <gene@ology.net>
2022-11-05 16:32:02 -0500

    Fix cut bounds

--- mojotarot.pl
+++ mojotarot.pl
@@ -187,7 +187,7 @@ __DATA__
   <input type="hidden" name="action" value="cut" />
   <select name="cut" title="Cut the deck" class="btn btn-sm" onchange="this.form.submit()">
     <option value="0" selected disabled>Cut</option>
-% for my $n (0 .. keys($deck->{cards}->%*) - 1) {
+% for my $n (0 .. keys($deck->{cards}->%*) - 2) {
     <option value="<%= $n %>"><%= $n %></option>
 % }
   </select>
