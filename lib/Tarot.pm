1b2ceb523aec124605225918088402d6cd2cfeaf
Gene Boggs <gene@ology.net>
2022-11-05 13:12:19 -0500

    Fix cut bounds

--- lib/Tarot.pm
+++ lib/Tarot.pm
@@ -85,7 +85,7 @@ sub cut_deck {
   my @cards = keys $deck->{cards}->%*;
   $n //= int(@cards) / 2; # default half of deck
   die "N must be between 0 and ", $#cards, "\n"
-    if $n < 0 || $n > @cards;
+    if $n < 0 || $n > $#cards;
   my @ordered = sort { $deck->{cards}{$a}{p} <=> $deck->{cards}{$b}{p} } @cards;
   my @cut = (
     @ordered[ $n .. $#ordered ],
